c80b48f3d49acd7037969d6e2cb149fc74ce2087
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-08-10 07:38:07 +0100

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,21 +5,56 @@
 from uuid import uuid4
 from datetime import datetime
 
+DATE_ISO8601_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
+    """
+    BaseModel class
 
-    def __init__(self):
-        """
-        BaseModel class constructor
+    Attributes:
+        id (str): unique id generated for the BaseModel instance
+        created_at (datetime): datetime object representing the time the
+        BaseModel instance was created
+        updated_at (datetime): datetime object representing the time the
+        BaseModel instance was updated
 
-        Attributes:
-            id (str): unique id of the BaseModel instance
-            created_at (datetime): creation date of the BaseModel instance
-            updated_at (datetime): update date of the BaseModel instance
+    Methods:
+        __init__(*args, **kwargs): initializes a BaseModel instance
+        __str__(): returns the string representation of the BaseModel instance
+        save(): updates the updated_at attribute with the current datetime
+        to_dict(): returns a dictionary containing all keys/values of the
+        BaseModel instance
+    """
+    def __init__(self, *args, **kwargs):
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        Initializes a BaseModel instance
+
+        Args:
+            *args: unused
+            **kwargs: keyword arguments passed to the constructor
+
+        Keyword Args:
+            id (str): unique id generated for the BaseModel instance
+            created_at (datetime): datetime object representing the time the
+            BaseModel instance was created
+            updated_at (datetime): datetime object representing the time the
+            BaseModel instance was updated
+
+        Returns:
+            None
+        """
+        if kwargs:
+            for key in kwargs:
+                if key != "__class__":
+                    setattr(self, key, kwargs[key])
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        kwargs[key], DATE_ISO8601_FORMAT))
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
