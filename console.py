88ed8e9d160c23d0b494f44203e27128c7079cfb
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-08-13 04:18:14 -0700

    Updating show() method in console.py

--- console.py
+++ console.py
@@ -4,13 +4,13 @@
 
 import cmd
 from models.base_model import BaseModel
-from models import storage
-from models.city import City
-from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.user import User
 from models.state import State
+from models import storage
+from models.city import City
+from models.amenity import Amenity
 import json
 import shlex
 
