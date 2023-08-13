5c055869609f8fa020c9396da6980534bbfe580a
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-08-13 04:49:48 -0700

    Updatng test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -11,47 +11,8 @@ from datetime import datetime
 AMENITY = amenity.Amenity
 
 
-class TestAmenityInstantiation(unittest.TestCase):
-    """Tests for instantiating the Amenity class"""
-
-    @classmethod
-    def set_up(cls):
-        """Set up for the doc tests"""
-        cls.amenity_f = inspect.getmembers(AMENITY, inspect.isfunction)
-
-    def test_amenity_module_docstring(self):
-        """Test for the amenity.py module docstring"""
-        self.assertIsNot(amenity.__doc__, None,
-                         "amenity.py needs a docstring")
-        self.assertTrue(len(amenity.__doc__) >= 1,
-                        "amenity.py needs a docstring")
-
-    def test_amenity_class_docstring(self):
-        """Test for the Amenity class docstring"""
-        self.assertIsNot(AMENITY.__doc__, None,
-                         "Amenity class needs a docstring")
-        self.assertTrue(len(AMENITY.__doc__) >= 1,
-                        "Amenity class needs a docstring")
-
-    def test_amenity_function_docstrings(self):
-        """Test for the presence of docstrings in Amenity methods"""
-        for function in self.amenity_f:
-            self.assertIsNot(function[1].__doc__, None,
-                             "{:s} method needs a docstring".format(function[0]))
-            self.assertTrue(len(function[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(function[0]))
-
-
 class TestAmenityAttributes(unittest.TestCase):
     """Tests for attributes of the Amenity class"""
-
-    def test_is_subclass_of_base_model(self):
-        """Test that Amenity is a subclass of BaseModel"""
-        amenity = AMENITY()
-        self.assertIsInstance(amenity, BaseModel)
-        self.assertTrue(hasattr(amenity, "id"))
-        self.assertTrue(hasattr(amenity, "created_at"))
-        self.assertTrue(hasattr(amenity, "updated_at"))
 
     def test_name_attribute_exists(self):
         """Test that Amenity has the 'name' attribute, and it's an empty string"""
@@ -79,11 +40,49 @@ class TestAmenityAttributes(unittest.TestCase):
         self.assertEqual(new_dict["created_at"], am.created_at.strftime(time_format))
         self.assertEqual(new_dict["updated_at"], am.updated_at.strftime(time_format))
 
+    def test_is_subclass_of_base_model(self):
+        """Test that Amenity is a subclass of BaseModel"""
+        amenity = AMENITY()
+        self.assertIsInstance(amenity, BaseModel)
+        self.assertTrue(hasattr(amenity, "id"))
+        self.assertTrue(hasattr(amenity, "created_at"))
+        self.assertTrue(hasattr(amenity, "updated_at"))
+
     def test_string_representation(self):
         """Test that the str method has the correct output"""
         amenity = AMENITY()
         string = "[Amenity] ({}) {}".format(amenity.id, amenity.__dict__)
         self.assertEqual(string, str(amenity))
+
+class TestAmenityInstantiation(unittest.TestCase):
+    """Tests for instantiating the Amenity class"""
+
+    @classmethod
+    def set_up(cls):
+        """Set up for the doc tests"""
+        cls.amenity_f = inspect.getmembers(AMENITY, inspect.isfunction)
+
+    def test_amenity_class_docstring(self):
+        """Test for the Amenity class docstring"""
+        self.assertIsNot(AMENITY.__doc__, None,
+                         "Amenity class needs a docstring")
+        self.assertTrue(len(AMENITY.__doc__) >= 1,
+                        "Amenity class needs a docstring")
+
+    def test_amenity_function_docstrings(self):
+        """Test for the presence of docstrings in Amenity methods"""
+        for function in self.amenity_f:
+            self.assertIsNot(function[1].__doc__, None,
+                             "{:s} method needs a docstring".format(function[0]))
+            self.assertTrue(len(function[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(function[0]))
+
+    def test_amenity_module_docstring(self):
+        """Test for the amenity.py module docstring"""
+        self.assertIsNot(amenity.__doc__, None,
+                         "amenity.py needs a docstring")
+        self.assertTrue(len(amenity.__doc__) >= 1,
+                        "amenity.py needs a docstring")
 
 
 if __name__ == "__main__":
